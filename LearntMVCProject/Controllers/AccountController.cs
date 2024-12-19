b862f940bff183210565ce5d5f592db8d3a63650
Ahmet Ülker <aulkerhm@gmail.com>
2024-12-19 16:05:28 +0300

    Login sayfasına başladım

---
+++ LearntMVCProject/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using LearntMVCProject.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using LearntMVCProject.Models;
+
+namespace LearntMVCProject.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+
+        }
+    }
+}
