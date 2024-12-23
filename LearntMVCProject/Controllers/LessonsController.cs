5eef0d84e17e1f7e75b761c5ebf2a80367a1de11
Ahmet Ülker <aulkerhm@gmail.com>
2024-12-23 16:05:40 +0300

    Bir çok yer düzenlendi

---
+++ LearntMVCProject/Controllers/LessonsController.cs
@@ -0,0 +1,32 @@
+﻿using LearntMVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LearntMVCProject.Controllers
+{
+    public class LessonsController : Controller
+    {
+        private readonly ILogger<LessonsController> _logger;
+
+        public LessonsController(ILogger<LessonsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Html()
+        {
+            return View();
+        }
+
+        public IActionResult Css()
+        {
+            return View();
+        }
+
+        public IActionResult Javascript()
+        {
+            return View();
+        }
+
+    }
+}
