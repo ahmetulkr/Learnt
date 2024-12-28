0aa2ea0607e5cdde2ad9dadfebfa39b0e6ff7c60
ahmetulkr <slausvac@gmail.com>
2024-12-29 01:33:42 +0300

    hesap giriş çıkış

--- LearntMVCProject/Controllers/AccountController.cs
+++ LearntMVCProject/Controllers/AccountController.cs
@@ -1,10 +1,27 @@
 ﻿using LearntMVCProject.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using LearntMVCProject.Models;
+
 
 namespace LearntMVCProject.Controllers
 {
-    
-    
+    public class AccountController : Controller
+    {   
+        private readonly UserManager<AppUser> userManager;  
+        private readonly SignInManager<AppUser> _signInManager;
+         private readonly  AppDbContext _context;
+         
+        
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+
+     public async Task<IActionResult> Login(LoginViewModel model)
+     {
+         
+     }
+    }
 }
