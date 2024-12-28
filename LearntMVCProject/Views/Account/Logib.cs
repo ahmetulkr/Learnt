3e3ed92cbb0b5045f5dd7b54934d666d02b5850e
ahmetulkr <slausvac@gmail.com>
2024-12-28 15:28:38 +0300

    aa

---
+++ LearntMVCProject/Views/Account/Logib.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace LearntMVCProject.Views.Account
+{
+    public class Logib : Controller
+    {
+        // GET: Logib
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
